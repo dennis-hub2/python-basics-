71a65617dffecf8bd1385ffd956f267d3b27bf48
dennis-hub2 <dennisankora17@gmail.com>
2023-03-30 16:07:13 +0000

    Logical Operators

---
+++ Arithemic Operators.py
@@ -0,0 +1,34 @@
+# """"
+# addition = +
+# subtraction = -
+# multiplication = *
+# power = **
+
+# Mathimatical Operator
+# PEMDAS (p=parentences e=exponent m=mutiplication d=division a=addition s=subtraction)
+
+# Logical Operators
+# and, or, <, >, <=, >=, ==, !=(not equal to), not
+# """
+
+# # a = 6
+# # # print(a==5)
+# # # print(a>5)
+# # print(a>=5)
+
+# gender = "Female"
+# age = 12
+# print(gender=="Male" or age==12)
+# print(gender=="Male" and age==12)
+# print(gender=="Female" or age!=12)
+
+#NOT>AND>OR
+
+#Conditional statments
+age = int(input("Age"))
+if age <= 0:
+  print("Invalid age")
+else: 
+  if age >= 18 and age <= 79:
+    print("You can drive")
+
---
+++ Control_Structures/if_statements.py
@@ -0,0 +1,7 @@
+""""
+Relational Operators.
+NOT(!)>AND(&&)>OR(||)
+"""
+a = 12
+b = 9
+print(a > b)
