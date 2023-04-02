3f32df76d63f3fdd5054eceecb0d7da782d1dd14
dennis-hub2 <dennisankora17@gmail.com>
2023-04-02 06:05:36 +0200

    login forms

--- README.md
+++
@@ -1,7 +0,0 @@
-# python-basics-
-name = "dennis ankora"
-phone_number = 0246885010
-age = 22
-print(name)
-print(phone_number)
-print(age)
---
+++ main.py
@@ -0,0 +1,7 @@
+first_name = "dennis"
+last_name = "Ankora"
+phone_number = 246885010
+
+print(first_name)
+print(last_name)
+print(phone_number)
